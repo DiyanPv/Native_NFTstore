d725619a64616e3ba6351bb3d07174c75b0be43a
DiyanPv <diyanpetrov61@gmail.com>
2023-01-13 01:28:20 +0200

    project setup

---
+++ App.js
@@ -0,0 +1,32 @@
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+const Stack = createStackNavigator();
+const theme = {
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    background: `transparent`
+  }
+}
+export const App = () => {
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Details" component={Details}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
