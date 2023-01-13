d6b6508a59cc3caffe19477a0dc95179fe2c3980
DiyanPv <diyanpetrov61@gmail.com>
2023-01-14 01:02:49 +0200

    basic setup

--- App.js
+++ App.js
@@ -1,32 +1,17 @@
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { useFonts } from "expo-font";
-const Stack = createStackNavigator();
-const theme = {
-  ...DefaultTheme,
-  colors:{
-    ...DefaultTheme.colors,
-    background: `transparent`
-  }
-}
-export const App = () => {
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+// import { useFonts } from "@expo-font";
+import { Home } from "./pages/Home";
+import { Details } from "./pages/Details";
+export default App = () => {
+  const Stack = createStackNavigator();
   return (
-    <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Details" component={Details}/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
